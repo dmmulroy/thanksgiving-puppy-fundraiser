771fca9884636e55dc8d6710c016a6a63c343751
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-11-25 15:11:52 -0500

    use zrange

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,32 +2,18 @@ export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
-import { nameToKey, puppyData, zrangeToObject } from "@/lib/utils";
+import { zrangeToObject } from "@/lib/utils";
 
 const redis = Redis.fromEnv();
 
-const puppyNames = puppyData.map(([name]) => name);
-
 export async function GET(_req: Request) {
 	try {
-		const puppyVotesV2 = await redis
+		const votesByName = await redis
 			.zrange("votes", 0, -1, {
 				withScores: true,
 			})
 			.then(zrangeToObject)
 			.catch(console.error);
-
-		console.log({ puppyVotesV2 });
-		const puppyVotes = await Promise.all(
-			puppyNames.map(async (name) => {
-				const key = nameToKey(name);
-				const votes = (await redis.zscore("votes", key)) ?? 0;
-
-				return [key, votes] as const;
-			}),
-		);
-
-		const votesByName = Object.fromEntries(puppyVotes);
 
 		console.log({ votesByName });
 

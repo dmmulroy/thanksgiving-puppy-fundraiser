483d835477125a9d151b91b7eaddb87f35a38987
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-11-25 09:33:24 -0500

    add x-

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -11,14 +11,16 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 export async function POST(req: Request) {
 	try {
 		const body = await req.text();
-		const headers = new Headers(req.headers);
-		const signature = headers.get("x-stripe-signature") ?? "";
+		const headersList = headers();
+		const signature = headersList.get("stripe-signature");
+		const xsignature = headersList.get("x-stripe-signature");
+		console.log({ signature, xsignature });
 		//const signature = headers().get("stripe-signature") as string;
 
 		let event: Stripe.Event;
 
 		try {
-			event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
+			event = stripe.webhooks.constructEvent(body, signature!, webhookSecret);
 		} catch (err) {
 			console.error("Webhook signature verification failed.", err);
 			return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
